1c864abc4d8cab45a3deffe2671c0290a58052b5
Kari Eronen <kari.eronen71@gmail.com>
2018-02-09 13:45:27 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import React from 'React';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY ='IzaSyCkrTzUOi117nt_fyZTNE5OXI7Fwl-4dao';
+
+const App = () => {
+return ( 
+
+	<div>
+           <SearchBar />
+	</div>
+       );
+}
+
+ReactDOM.render(</App />,document.querySelector('.container'))
+
